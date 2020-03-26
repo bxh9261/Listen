1da1f1d66f5e9a4e01148422a05dd989bc3ca6d6
brad <bxh9261@g.rit.edu>
2020-03-26 00:35:57 -0400

    +First Unity project setup and prototype build

---
+++ TPW-Group-3-Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] platforms;
+    public GameObject hands;
+    public GameObject dude;
+    int[] marks = new int[] {3,5,5,2,1,2,3,4,5,3,1};
+    int currPlatform = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDown(hands);
+
+        for(int i = 0; i < platforms.Length; i++)
+        {
+            moveDown(platforms[i]);
+        }
+        
+        if(dude.transform.position.y > -4.4)
+        {
+            moveDown(dude);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            if(platforms[currPlatform].transform.position.y < -1.5)
+            {
+                dude.transform.position = new Vector3(platforms[currPlatform].transform.position.x, platforms[currPlatform].transform.position.y + 0.5f, platforms[currPlatform].transform.position.z);
+                currPlatform++;
+            }
+        }
+    }
+
+    private void moveDown(GameObject go)
+    {
+        go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y - 0.01f, go.transform.position.z);
+    }
+}
