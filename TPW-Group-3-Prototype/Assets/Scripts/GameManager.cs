34776117e53a9850e9ca80832b1609896ec44ebc
brad <bxh9261@g.rit.edu>
2020-04-23 17:54:40 -0400

    +Added Example video and Words on Platforms

--- TPW-Group-3-Prototype/Assets/Scripts/GameManager.cs
+++ TPW-Group-3-Prototype/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameManager : MonoBehaviour
     public List<GameObject> numberplatforms;
     public GameObject dude;
     int[] canthelp = new int[] {1,5,1,0,4,3,2,0,6,7,1,2,3,2,1};
+    string[] canthelpWords = new string[] { "wise", "men", "say", "", "fools", "rush", "in", "", "I", "can't", "help", "falling", "love", "with", "you" };
     float[] xNotes = new float[8];
     public GameObject[] numbers = new GameObject[8];
     int currPlatform = 0;
@@ -18,6 +20,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+
         //int counterdebug = 0;
         platforms = new List<GameObject>();
         for (int i = 0; i < 8; i++) {
@@ -27,8 +31,11 @@ public class GameManager : MonoBehaviour
         {
             if(canthelp[j] > 0)
             {
-                platforms.Add(Instantiate(platObj, new Vector3(xNotes[canthelp[j]], 8 + (2.1f * j), 0), Quaternion.identity));
-                numberplatforms.Add(Instantiate(numbers[canthelp[j]], new Vector3(-5.0f, 8 + (2.1f * j), 0), Quaternion.identity));
+                GameObject word = Instantiate(platObj, new Vector3(xNotes[canthelp[j]], 8 + (2.0f * j), 0), Quaternion.identity);
+                word.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = canthelpWords[j];
+                platforms.Add(word);
+
+                numberplatforms.Add(Instantiate(numbers[canthelp[j]], new Vector3(-5.0f, 8 + (2.0f * j), 0), Quaternion.identity));
             }
             
             //Debug.Log(counterdebug);
@@ -63,8 +70,14 @@ public class GameManager : MonoBehaviour
             if(platforms[currPlatform].transform.position.y < -1.5)
             {
                 dude.transform.position = new Vector3(platforms[currPlatform].transform.position.x, platforms[currPlatform].transform.position.y + 0.5f, platforms[currPlatform].transform.position.z);
+                platforms[currPlatform].transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = "";
                 currPlatform++;
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("TPW Game");
         }
        // if(rHand.Length == 0)
         //{
@@ -80,6 +93,7 @@ public class GameManager : MonoBehaviour
 
     private void moveDown(GameObject go)
     {
-        go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y - 0.01f, go.transform.position.z);
+        float translation = Time.deltaTime * 1.15f;
+        go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y - translation, go.transform.position.z);
     }
 }
